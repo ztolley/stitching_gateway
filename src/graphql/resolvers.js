d3dfa86de843664de42c6df0f83979a7b8c9cc3a
Zac Tolley <zac@thetolleys.com>
2020-06-07 21:01:39 +0000

    Manually fetch data for join

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,19 +1,27 @@
-const { delegateToSchema } = require('@graphql-tools/delegate')
+const { graphql } = require('graphql')
 
 const truckFbrTruckResolver = (fdpSchema) => ({
   selectionSet: '{ id }',
-  resolve(truck, args, context, info) {
-    return delegateToSchema({
-      schema: fdpSchema,
-      option: 'query',
-      fieldName: 'FBRTruck',
-      args: {
-        ...args,
-        id: truck.id,
-      },
-      context,
-      info,
-    })
+  async resolve(truck) {
+    const query = `
+      query {
+        dataSources(dataType: Fbr) {
+          ... on FBRDataSource {
+            fbrTruck(truckId: "${truck.id}") {
+              uuid
+              truckName
+              burnRates {
+                speed
+                mpg
+              }
+            }
+          }
+        }
+      }
+    `
+
+    const response = await graphql(fdpSchema, query)
+    return response.data.dataSources[0].fbrTruck[0]
   },
 })
 
