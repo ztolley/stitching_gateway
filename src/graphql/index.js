7b7ba2dce76c99eb4c7198c807a31381204f556a
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-07-28 07:03:35 -0400

    refactor

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -6,6 +6,49 @@ const { Kind, GraphQLList } = require('graphql')
 const fdpSchema = require('./fdpSchema')
 const planSchema = require('./planSchema')
 
+function forwardToFbr(truckId, selectionSet) {
+  return {
+    kind: Kind.SELECTION_SET,
+    selections: [
+      {
+        kind: Kind.INLINE_FRAGMENT,
+        typeCondition: {
+          kind: Kind.NAMED_TYPE,
+          name: {
+            kind: Kind.NAME,
+            value: 'FBRDataSource',
+          },
+        },
+        selectionSet: {
+          kind: Kind.SELECTION_SET,
+          selections: [
+            {
+              kind: Kind.FIELD,
+              name: {
+                kind: Kind.NAME,
+                value: 'fbrTruck',
+              },
+              arguments: [
+                {
+                  kind: Kind.ARGUMENT,
+                  name: {
+                    kind: Kind.NAME,
+                    value: 'truckId',
+                  },
+                  value: {
+                    kind: Kind.STRING,
+                    value: truckId,
+                  },
+                },
+              ],
+              selectionSet,
+            },
+          ],
+        },
+      },
+    ],
+  }
+}
 async function getSchema() {
   return stitchSchemas({
     subschemas: [
@@ -25,70 +68,38 @@ async function getSchema() {
           FBRTruck: {
             selectionSet: `{ uuid }`,
             resolve: (originalResult, context, info, schema, selectionSet) => {
-              let results = delegateToSchema({
+              const datasources = delegateToSchema({
                 schema,
                 operation: 'query',
                 fieldName: 'dataSources',
                 returnType: new GraphQLList(fdpSchema.getType('FBRDataSource')),
                 args: { dataType: 'Fbr' },
-                selectionSet: {
-                  kind: Kind.SELECTION_SET,
-                  selections: [
-                    {
-                      kind: Kind.INLINE_FRAGMENT,
-                      typeCondition: {
-                        kind: Kind.NAMED_TYPE,
-                        name: {
-                          kind: Kind.NAME,
-                          value: 'FBRDataSource',
-                        },
-                      },
-                      selectionSet: {
-                        kind: Kind.SELECTION_SET,
-                        selections: [
-                          {
-                            kind: Kind.FIELD,
-                            name: {
-                              kind: Kind.NAME,
-                              value: 'fbrTruck',
-                            },
-                            arguments: [
-                              {
-                                kind: Kind.ARGUMENT,
-                                name: {
-                                  kind: Kind.NAME,
-                                  value: 'truckId',
-                                },
-                                value: {
-                                  kind: Kind.STRING,
-                                  value: originalResult.uuid,
-                                },
-                              },
-                            ],
-                            selectionSet,
-                          },
-                        ],
-                      },
-                    },
-                  ],
-                },
+                selectionSet: forwardToFbr(originalResult.uuid, selectionSet),
                 context,
                 info,
                 skipTypeMerging: true,
               })
-              if (!Array.isArray(results)) {
-                return results
+              
+              if (!Array.isArray(datasources)) {
+                return datasources
               }
-              results = handleResult(
-                results[0].fbrTruck,
-                getErrors(results[0], 'fbrTruck'),
+
+              const datasource = datasources[0]
+              const fbrTrucks = handleResult(
+                datasource.fbrTruck,
+                getErrors(datasource, 'fbrTruck'),
                 schema,
                 context,
                 info,
                 new GraphQLList(fdpSchema.getType('FBRTruck')),
                 true,
               );
-              return Array.isArray(results) ? results[0] : results;
+
+              if (!Array.isArray(fbrTrucks)) {
+                return fbrTrucks
+              }
+
+              return fbrTrucks[0];
             },
           },
         },
