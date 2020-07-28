571c38d50016398a84040af83f5d8a96209a282d
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-07-28 06:46:37 -0400

    looks like we actually export the helper methods we need, in a future version the getErrors API may change and it makes sense to move into the delegate package, but for right now should be great

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,6 @@
 const { stitchSchemas } = require('@graphql-tools/stitch')
-const { delegateToSchema } = require('@graphql-tools/delegate')
+const { delegateToSchema, handleResult } = require('@graphql-tools/delegate')
+const { getErrors } = require('@graphql-tools/utils')
 const { Kind, GraphQLList } = require('graphql')
 
 const fdpSchema = require('./fdpSchema')
@@ -24,7 +25,7 @@ async function getSchema() {
           FBRTruck: {
             selectionSet: `{ uuid }`,
             resolve: (originalResult, context, info, schema, selectionSet) => {
-              const results = delegateToSchema({
+              let results = delegateToSchema({
                 schema,
                 operation: 'query',
                 fieldName: 'dataSources',
@@ -75,23 +76,19 @@ async function getSchema() {
                 info,
                 skipTypeMerging: true,
               })
-              // The code below is a hack that reannotates the fbrTruck with the subschema metadata
-              // Improperly, not preserving errors.
-              // Helper functions could be created that do this properly.
-              // Alternatively, schema transforms could be created that take care of this.
-              // schema transforms operate on the original result prior to annotation of data with errors
-              // and would not require special helper functions.
-              // The HoistField transform is an attempt along this line with object fields only.
-              //
-              // Note that the array check itself is valid, for if no data was returned,
-              // the returnes results will contain an error that can be passed through.
-              // It is the object spread that is invalid, as it simply hoists the nested data
-              // into the result containing appropriate subschema metadata, but does not adjust
-              // any error annotations, which is unfortunately required. 
-              return Array.isArray(results) ? {
-                ...results[0],
-                ...results[0].fbrTruck[0]
-              } : result; 
+              if (!Array.isArray(results)) {
+                return results
+              }
+              results = handleResult(
+                results[0].fbrTruck,
+                getErrors(results[0], 'fbrTruck'),
+                schema,
+                context,
+                info,
+                new GraphQLList(fdpSchema.getType('FBRTruck')),
+                true,
+              );
+              return Array.isArray(results) ? results[0] : results;
             },
           },
         },
